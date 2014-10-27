dbaeceb3239261b3e797695041820fcb0c98a05f
Jerry <jjzhu6@hotmail.com>
2014-10-27 22:03:11 +0800

    Add sample question types: Radio, Select and Text.

--- AngularDynamicForm/Scripts/Controllers/TestFormController.js
+++ AngularDynamicForm/Scripts/Controllers/TestFormController.js
@@ -2,6 +2,66 @@
 
 dynamicFormApp.controller('TestFormController',
     function TestFormController($scope) {
-        
+        var questions = [
+            {
+                Name: "Question1",
+                Label: "Please answer my question 1",
+                Type: "Text",
+                Include: "/Scripts/Templates/QuestionTypes/Text.html",
+                Value: ""
+            },
+            {
+                Name: "Question2",
+                Label: "Please select for my question 2",
+                Type: "Select",
+                Include: "/Scripts/Templates/QuestionTypes/Select.html",
+                Value: "",
+                Options: [
+                    {
+                        Value: "Select1",
+                        Label: "This is Select 1"
+                    },
+                    {
+                        Value: "Select2",
+                        Label: "This is Select 2"
+                    },
+                    {
+                        Value: "Select3",
+                        Label: "This is Select 3"
+                    },
+                    {
+                        Value: "Select4",
+                        Label: "This is Select 4"
+                    }
+                ]
+            },
+            {
+                Name: "Question3",
+                Label: "Please check my question 3",
+                Type: "Radio",
+                Include: "/Scripts/Templates/QuestionTypes/Radio.html",
+                Value: "",
+                Options: [
+                    {
+                        Value: "Radio1",
+                        Label: "This is Radio 1"
+                    },
+                    {
+                        Value: "Radio2",
+                        Label: "This is Radio 2"
+                    },
+                    {
+                        Value: "Radio3",
+                        Label: "This is Radio 3"
+                    },
+                    {
+                        Value: "Radio4",
+                        Label: "This is Radio 4"
+                    }
+                ]
+            }
+        ];
+
+        $scope.questions = questions;
     }
 );
