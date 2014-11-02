2a562f94771840f537232787f0a7150e1e398e81
Jerry <jjzhu6@hotmail.com>
2014-11-02 20:29:01 +0800

    Relocate javascript library files.

--- AngularDynamicForm/App_Start/BundleConfig.cs
+++ AngularDynamicForm/App_Start/BundleConfig.cs
@@ -9,19 +9,19 @@ namespace AngularDynamicForm
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/lib/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/lib/jquery-ui-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/lib/jquery.unobtrusive*",
+                        "~/Scripts/lib/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/lib/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
@@ -40,10 +40,10 @@ namespace AngularDynamicForm
                         "~/Content/themes/base/jquery.ui.theme.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                        "~/Scripts/angular.js",
-                        "~/Scripts/angular-route.js",
-                        "~/Scripts/angular-resource.js",
-                        "~/Scripts/angular-sanitize.js",
+                        "~/Scripts/lib/angular.js",
+                        "~/Scripts/lib/angular-route.js",
+                        "~/Scripts/lib/angular-resource.js",
+                        "~/Scripts/lib/angular-sanitize.js",
                         "~/Scripts/apps/dynamic-form-app.js",
                         "~/Scripts/Repositories/QuestionRepository.js",
                         "~/Scripts/Controllers/TestFormController.js"
