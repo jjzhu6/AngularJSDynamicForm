107045763fa6727be431be032b298c778f1b2d59
Jerry <jjzhu6@hotmail.com>
2014-11-09 12:04:55 +0800

    Display responses link more clear.

--- AngularDynamicForm/Scripts/Controllers/FormController.js
+++ AngularDynamicForm/Scripts/Controllers/FormController.js
@@ -1,17 +1,22 @@
 ﻿'use strict';
 
-dynamicFormApp.controller('FormController', function FormController($scope, $location, questionRepository) {
+dynamicFormApp.controller('FormController', function FormController($scope, $route, $location, questionRepository) {
         $scope.formId = $location.search().id;
 
         questionRepository.form.get({ id: $scope.formId }).$promise.then(function (data) {
             $scope.form = data;
         });
         
-        $scope.responseText = "(See Responses)"
+        questionRepository.form.getResponseList({ formId: $scope.formId }).$promise.then(function (data) {
+            $scope.responsesCount = data.length;
+            $scope.responseText = "(See " + $scope.responsesCount + " Response/s)";
+        });
 
         $scope.save = function (form, formName) {
             if (formName.$valid) {
-                questionRepository.form.save(form);
+                questionRepository.form.save(form).$promise.then(function () {
+                    $route.reload();
+                });
             }
         }
     }
